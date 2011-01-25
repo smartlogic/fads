7813c6f8dbda11bcca0f69da35a6933dfdefe671
Ed Schmalzle <Jonas714@gmail.com>
2011-01-25 15:51:24 -0500

    add generic model steps based on factories

---
+++ features/step_definitions/model_steps.rb
@@ -0,0 +1,10 @@
+Factory.factories.each do |name, factory|
+  Given /^I have a #{name}:$/ do |attrs_table|
+    attrs = {}
+    attrs_table.raw.each do |(attr, value)|
+      sanitized_attr = attr.gsub(/\s+/, "-").underscore
+      attrs[sanitized_attr.to_sym] = value
+    end
+    Factory(name, attrs)
+  end
+end

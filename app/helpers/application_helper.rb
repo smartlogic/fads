42544c808a9588029673f25e6c682f64dd9a9af9
John Trupiano <jtrupiano@gmail.com>
2011-01-27 19:42:04 -0500

    Use common styles (subnav and form) when creating clients

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     selected = client ? client.id : nil
     options_for_select(options, selected)
   end
+
+
+  def subnav(selected)
+    render :partial => 'subnav', :locals => { :selected => selected }
+  end
+
+  def subnav_class(link, selected)
+    (link == selected) ? 'selected' : ''
+  end
 end
--- app/helpers/creatives_helper.rb
+++ app/helpers/creatives_helper.rb
@@ -1,5 +1,2 @@
 module CreativesHelper
-  def subnav(selected)
-    render :partial => 'subnav', :locals => { :selected => selected }
-  end
 end

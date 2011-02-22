8ccc976157fc9365422c5b6a762729685e225011
Ed Schmalzle <ed@nerded.net>
2011-02-22 10:30:28 -0500

    add wireframes controller

---
+++ app/controllers/wireframes_controller.rb
@@ -0,0 +1,14 @@
+class WireframesController < ApplicationController
+
+ # Use this when we have more than one layout
+ # layout :choose_layout
+
+ # def choose_layout
+ #   case action_name
+ #   when *%w(admin_action another_admin_action)
+ #     'fads_admin'
+ #   else
+ #     false
+ #   end
+ # end
+end
